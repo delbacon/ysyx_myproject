c6ed0d2da5512aa4bf68bce473fd1ecdc8351ac9
241220000-Zhang Peixiao <1612646337@qq.com>
2026-02-04 16:44:22 +0800

    E5

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -10,7 +10,7 @@
 static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* TOPNAME);
-/*
+
 static void single_cycle() {
   dut.clk = 0; dut.eval();
   dut.clk = 1; dut.eval();
@@ -21,16 +21,16 @@ static void reset(int n) {
   while (n -- > 0) single_cycle();
   dut.rst = 0;
 }
-*/
+
 int main() {
   nvboard_bind_all_pins(&dut);
   nvboard_init();
 
-  //reset(10);
+  reset(10);
 
   while(1) {
     dut.eval();
     nvboard_update();
-  //  single_cycle();
+    single_cycle();
   }
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,9 +1,31 @@
 module top(
-  input a,
-  input b,
-  output f,
-  output test
+  input clk,
+  input rst,
+  input sw,
+  output reg [15:0] led,
+  output reg [31:0]cnt
 );
-  assign f = a ^ b;
-  assign test = 1;
+
+
+always@(posedge clk or posedge rst)begin
+  if(rst)begin
+    cnt <= 'd0;
+  end else if(cnt == 'd10_000_00 - 1)begin
+    cnt <= 'd0;
+  end else begin
+    cnt <= cnt + 1;
+  end
+end
+
+always@(posedge clk or posedge rst)begin
+  if(rst)begin
+    led <= 'd0;
+  end else if(cnt == 'd10_000_00 - 1) begin
+    if(led == 'd0)begin
+      led <= 'd1;
+    end else begin
+      led <= {led[14:0],led[15]};
+    end
+  end
+end
 endmodule
