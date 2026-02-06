d3138061698e1c5b2b87f53b923f3257b378de9b
tracer-ysyx <tracer@ysyx.org>
2026-02-06 10:06:09 +0800

    > compile NEMU ysyx_22040000 张培枭 Linux delbacon-GLO-GXXX 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:06:09 up 20:56,  1 user,  load average: 2.32, 3.26, 3.70

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -365,7 +365,7 @@ static word_t eval(int p,int q, bool *legal) {
       word_t val = calc2op(val1, tokens[op].type, val2, legal);
       return val;
     }else {
-      word_t val = calc1op(tokens[op].type, val1, legal);
+      word_t val = calc1op(tokens[op].type, val2, legal);
       return val;
     }
 
