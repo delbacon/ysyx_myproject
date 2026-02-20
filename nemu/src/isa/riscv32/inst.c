ca6d5ae941ec4605deadca2f15c01cc5f8760389
tracer-ysyx <tracer@ysyx.org>
2026-02-20 12:10:34 +0800

    > compile NEMU ysyx_22040000 张培枭 Linux delbacon-GLO-GXXX 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:10:34 up 21:52,  1 user,  load average: 1.22, 1.45, 1.39

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -22,26 +22,39 @@
 #define Mr vaddr_read
 #define Mw vaddr_write
 
+//enum存储riscv六种指令类型
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_R, TYPE_I, TYPE_S,TYPE_B, TYPE_U, TYPE_J,
   TYPE_N, // none
 };
 
+//define读取立即数的值,并将相应的寄存器值保存在src1和src2中
+//---------------------------------------------//
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
+
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immB() do { *imm = ((SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1) | 0 ); } while(0)
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immJ() do { *imm = ((SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1) | 0); } while(0)
+
+//---------------------------------------------//
+
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
+  //因为RISCV指令集rs1,rs2和rd位置确定，所以先统一读取，再通过switch判断所属指令类型，然后对症下药
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
+    case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
-    case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_U:                   immU(); break;
+    case TYPE_J:                   immJ(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
@@ -57,11 +70,18 @@ static int decode_exec(Decode *s) {
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
-
+//通过opcode的格式进行匹配，0匹配0,1匹配1,？匹配0和1
+//INSTPAT(模式字符串, 指令名称, 指令类型, 指令执行操作)
+//指令名称作为注释，指令类型用于后续译码
   INSTPAT_START();
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->snpc; s->dnpc = imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(rd) = s->snpc; s->dnpc = (src1 + imm) & ~1);
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
+
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
@@ -71,8 +91,9 @@ static int decode_exec(Decode *s) {
 
   return 0;
 }
-
+//作用是从内存中取出一条指令，并且让snpc指向下一条指令的位置
 int isa_exec_once(Decode *s) {
   s->isa.inst = inst_fetch(&s->snpc, 4);
+  //进入译码
   return decode_exec(s);
 }
