7491a342de6740198e341a9360ea15a0dc1975ac
tracer-ysyx <tracer@ysyx.org>
2026-02-21 23:55:32 +0800

    > compile NEMU ysyx_22040000 张培枭 Linux delbacon-GLO-GXXX 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:55:32 up 14:02,  1 user,  load average: 0.74, 1.21, 1.16

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -26,7 +26,7 @@ $(BINARY):: compile_git
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
-
+# override ARGS += -b
 # Command to execute NEMU
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
