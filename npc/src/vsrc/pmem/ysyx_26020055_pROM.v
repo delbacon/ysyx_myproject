a42e1cd7965e49d43bd7b2b59af5b3c903e85d47
ysyx_26020055-Zhang Peixiao <1612646337@qq.com>
2026-03-18 09:59:28 +0800

    写 aix4-lite 过程中的存档,目前写完了 ROM 方的状态机,正在写 IFU ,还没有测试

--- npc/src/vsrc/pmem/ysyx_26020055_pROM.v
+++ npc/src/vsrc/pmem/ysyx_26020055_pROM.v
@@ -2,77 +2,197 @@
 
 import "DPI-C" function int pROM_read_HDL(int addr) ;
 // ROM 必定按 word 读，直接设置为 32 bit
+/*
 module ysyx_26020055_pROM #(ADDR_WIDTH = 25, DATA_WIDTH = 32) (
     input                      clk                ,
-    /* verilator lint_off UNUSEDSIGNAL */
     input                      rst                ,
     input [31:0]               vaddr              ,
     input                      reqValid           ,
     output reg                 respValid          ,
     input                      respReady          ,
     output                     reqReady           ,
-    output reg[DATA_WIDTH-1:0] rdata                    
+    output reg[DATA_WIDTH-1:0] rdata               
+*/
+module ysyx_26020055_pROM (
+    input               clk         ,
+    input               rst         ,
+    input        [31:0] wmask       ,  
+            /* AXI4-lite */
+    /* 读地址操作 */
+    input        [31:0] arAddr      ,
+    input               arValid     ,
+    output reg          arReady     ,
+    /* 读数据操作 */
+    output reg   [31:0] rData       ,
+    output reg          rResp       ,
+    output reg          rValid      ,
+    input               rReady      ,
+    /* 写地址操作 */
+    input        [31:0] awAddr      ,
+    input               awValid     ,
+    output              awReady     ,
+    /* 写数据操作 */
+    input        [31:0] wData       ,
+    input               wValid      ,
+    output              wReady      ,
+    /* 写回操作 */
+    output              bResp       ,
+    output              bValid      ,
+    input               bReady  
 );
-//寄存器组
-    reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
 
+// ================== 设置不用的端口为0 ===================== //
+// aw ,w ,b 不处理
+    assign awReady = 1'b0;
+    assign wReady  = 1'b0;
+    assign bResp   = 1'b0;
+    assign bValid  = 1'b0;
+// ======================================================== //
+
+// ==================== 寄存器组 =========================== //
+// 32bit 地址， 8bit 单元
+    //localparam ADDR_WIDTH = 27;//27: 0x800_0000 大小(128MB)
+    localparam ADDR_WIDTH = 10;//27: 0x000_1000 大小(1KB)
+    localparam DATA_WIDTH = 8;
+
+    localparam VADDR_INIT = 32'h8000_0000;
+    reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
+// 初始化
     initial begin
-        for(int i=0;i<28'h800_0000;i=i+4)begin
-            rf[i>>2] = pROM_read_HDL(i+32'h8000_0000);
+        for(int i=0;i<2**ADDR_WIDTH-1;i=i+4)begin
+            {rf[i+3],rf[i+2],rf[i+1],rf[i]} = pROM_read_HDL(i + VADDR_INIT);
         end
     end
+// ======================================================== //
 
-    wire [ADDR_WIDTH-1:0] addr;
-    assign addr = vaddr[ADDR_WIDTH-1:0];
+
+// ======================= 操作mem' ======================== //
+// arAddr_reg
+// 目前只有一个寄存空间，后续可以使用 fifo 扩展
+    reg [31:0] arAddr_reg;
+    always@(posedge clk)begin
+        if(rst)
+            arAddr_reg <= 32'h0;
+        else if(arValid && arReady) // 只有握手成功时认为读地址有效
+            arAddr_reg <= arAddr;
+    end
+// 获得物理地址
+// 因为只差一个 28bit ，直接截取就可以
+    wire [ADDR_WIDTH-1:0] arPaddr;
+    assign arPaddr = arAddr_reg[ADDR_WIDTH-1:0];
+
+    reg arAddr_reg_valid; // arAddr_reg 是否有效 
+    always@(posedge clk)begin
+        if(rst)
+            arAddr_reg_valid <= 1'b0;
+        else if(arValid && arReady)
+            arAddr_reg_valid <= 1'b1;
+        else
+            arAddr_reg_valid <= 1'b0;
+    end
 
 //读
+localparam DEVICE_ADDR = 32'h1000_0000;
+localparam DEVICE_SIZE = 32'h0000_1000;
+
 always @(posedge clk) begin
-    if(reqValid)begin
-        if(vaddr >= 32'h1000_0000 && vaddr < 32'h1000_1000) rdata <= pROM_read_HDL(vaddr);
-        else rdata <= rf[addr>>2];
+    if(arAddr_reg_valid)begin
+        // device
+        if(arAddr >= DEVICE_ADDR && arAddr < DEVICE_ADDR+DEVICE_SIZE)begin
+            rData <= pROM_read_HDL(arAddr);
+            rResp <= 2'b00;
+        // mem
+        end else if(arAddr <= 2**ADDR_WIDTH-4)begin 
+            rData <= {rf[arPaddr+3],rf[arPaddr+2],rf[arPaddr+1],rf[arPaddr]};
+            rResp <= 2'b00;//有效地址
+        end else begin
+            rData <= rData;
+            rResp <= 2'b10;//无效地址
+        end
     end
 end
 
-// ============== 状态机 =================//
-localparam IDLE = 2'b00,
-           BUSY = 2'b01,
-           WAITREADY = 2'b10;
+// 随机延迟生成0-31,模拟读出延迟
+// rValid 从设备声明数据有效的延迟，从读地址寄存器有效时开始计算
+// 这里模拟随机时刻后 mem 成功读出数据，并送到 rData 上，且置 rValid 为 1
+    wire rValid_base;
+    ysyx_26020055_lfsr uROM_ysyx_26020055_lfsr (
+        .clk            (clk             ),
+        .rst            (rst             ),
+        .start          (arAddr_reg_valid),
+        .output_pulse   (rValid_base     )
+    );
+// ========================== 状态机 ============================//
+// 状态机1:for data
+localparam IDLE = 2'b00,//待命
+           BUSY = 2'b01,//工作中
+           WAITREADY = 2'b10;//等待 master 准备完成
 reg [1:0] st,n_st;
 always@(posedge clk)begin
     if(rst)
         st <= IDLE;
-    else
+    elsey
         st <= n_st;
 end
 always@(*)begin
     case(st)
-    // 如果收到 master 的读请求，则开始工作
-        IDLE:  begin n_st = (reqValid)?BUSY:IDLE; end
-        BUSY:  begin n_st = (respValid_base)?WAITREADY:BUSY; end
-    // 自身工作完成后，开始等待 master 自身工作完成
-        WAITREADY:begin   n_st = (respReady)?IDLE:WAITREADY;   end
+    // 如果收到 master 的有效地址，则开始工作
+        IDLE:     begin n_st = (arValid)?BUSY:IDLE;          end
+    // 等待自身工作完成
+        BUSY:     begin n_st = (rValid_base)?WAITREADY:BUSY; end
+    // 自身工作完成后，开始等待 master 工作完成以传输数据
+        WAITREADY:begin   n_st = (rReady)?IDLE:WAITREADY;    end
         default : n_st = IDLE;
     endcase
 end
-wire respValid_base;
-assign respValid = (st == WAITREADY || respValid_base);
+// WAITREADY 时自身工作已经完成，输出数据有效，设置 rValid 为 1，并且等待 master 的 READY 信号
+    assign rValid  = (st == WAITREADY);
+
+// ============================ //
+//状态机2 for addr
+localparam ADDR_IDLE = 2'b00,//待命
+           ADDR_BUSY = 2'b01,//工作中
+           ADDR_WAITREADY = 2'b10;//等待 master 准备完成
+reg [1:0] ad_st,ad_n_st;
+always@(posedge clk)begin
+    if(rst)
+        ad_st <= ADDR_IDLE;
+    elsey
+        ad_st <= ad_n_st;
+end
+always@(*)begin
+    case(ad_st)
+    // 如果收到 master 的有效地址，则开始工作
+        ADDR_IDLE:     begin ad_n_st = (arValid)?ADDR_BUSY:ADDR_IDLE;          end
+    // 模拟消费地址的延迟
+        ADDR_BUSY:     begin ad_n_st = (arReady_base)?ADDR_IDLE:ADDR_BUSY;     end
+        default : ad_n_st = ADDR_IDLE;
+    endcase
+end
+
+// IDLE 时自身工作已经完成，等待 master 的有效地址
+    assign arReady = (ad_st == ADDR_IDLE);
+    wire arReady_base;
+// 这里模拟消费地址的延迟
+    ysyx_26020055_lfsr uROM_reqReady_ysyx_26020055_lfsr (
+        .clk            (clk             ),
+        .rst            (rst             ),
+        .start          (arAddr_reg_valid),
+        .output_pulse   (arReady_base    )
+    );
 
 
 
-// 随机延迟生成0-15
-ysyx_26020055_lfsr uROM_ysyx_26020055_lfsr (
-    .clk            (clk            ),
-    .rst            (rst            ),
-    .start          (reqReady       ),
-    .output_pulse   (respValid_base )
-);
 
-ysyx_26020055_lfsr uROM_reqReady_ysyx_26020055_lfsr (
-    .clk            (clk            ),
-    .rst            (rst            ),
-    .start          (reqValid       ),
-    .output_pulse   (reqReady       )
-);
+
+
+
+
+
+
+
+
+
 
 /*
 // for test 测试用可控延迟生成
--- npc/src/vsrc/ysyx_26020055_IFU.v
+++ npc/src/vsrc/ysyx_26020055_IFU.v
@@ -41,7 +41,7 @@ reg [31:0] ifu_data,ifu_addr;
 wire mem_ready;
 assign mem_ready = (rom_st == MEM_BUSY && ifu_respValid);
 // 如果取指完成，取出的指令
-assign inst = mem_ready?ifu_data:32'b0;
+assign inst = mem_ready?ifu_rData:32'b0;
 // 此时 ifu 工作完成，inst和pc有效
 assign ifu_valid = mem_ready;
 assign ifu_addr = pc;
@@ -58,6 +58,7 @@ end
 
 // ============== ROM 状态机 ====================//
 // 用于和 ROM 交互
+// addr 状态机
 localparam  MEM_IDLE = 'd0,
             MEM_WAIT = 'd1,
             MEM_BUSY = 'd2;
@@ -72,18 +73,65 @@ always@(*)begin
     case(rom_st)
         // 如果主状态机开始等待 mem 执行
         MEM_IDLE: begin rom_n_st = (st == WAITMEM)?MEM_WAIT:MEM_IDLE; end
-        // 等待 ROM 不忙，可以响应 IFU 的请求（reqReady），期间一直向 mem 发出读请求(ifu_reqValid)
-        MEM_WAIT: begin rom_n_st = (ifu_reqReady)?MEM_BUSY:MEM_WAIT; end
         // 等待 ROM 取完毕
         MEM_BUSY:begin rom_n_st = (ifu_respValid)?MEM_IDLE:MEM_BUSY;end
     endcase
 end
 
+// data 状态机
+localparam  MEM_IDLEDATA = 'd0,
+            MEM_WAITDATA = 'd1;
+reg [1:0]data_st,data_n_st;
+always@(posedge clk)begin
+    if(rst)
+        data_st <= MEM_IDLEDATA;
+    else
+        data_st <= data_n_st;
+end
+always@(*)begin
+    case(data_st)
+        // 如果主状态机开始等待 mem 执行
+        MEM_IDLEDATA: begin rom_n_st = (st == WAITMEM)?MEM_WAITDATA:MEM_IDLEDATA; end
+        // 等待 ROM 不忙，可以响应 IFU 的请求（reqReady），期间一直向 mem 发出读请求(ifu_reqValid)
+        MEM_WAITDATA: begin rom_n_st = (ifu_rValid)?MEM_IDLEDATA:MEM_WAITDATA; end
+    endcase
+end
+// wait data 时一直置 1 ，表示准备好接收数据
+assign ifu_rReady = (st==MEM_WAITDATA);
 assign ifu_reqValid = (rst_ready)?1:(rom_st == MEM_WAIT);
 
+reg [31:0] ifu_arAddr  ;
+wire ifu_arValid       ;
+wire ifu_arReady       ;
+reg [31:0] ifu_rData   ;
+wire ifu_rResp         ;
+wire ifu_rValid        ;
+wire ifu_rReady        ;
 
-wire ifu_reqValid,ifu_respValid;
-wire ifu_reqReady,ifu_respReady;
+ysyx_26020055_pROM u_ysyx_26020055_pROM (
+    .clk        (clk           ),
+    .rst        (rst           ),
+    .wmask      (32'd0         ),  
+    .arAddr     (ifu_arAddr    ),
+    .arValid    (ifu_arValid   ),
+    .arReady    (ifu_arReady   ),
+    .rData      (ifu_rData     ),
+    .rResp      (ifu_rResp     ),
+    .rValid     (ifu_rValid    ),
+    .rReady     (ifu_rReady    ),
+
+    .awAddr     (32'd0         ),
+    .awValid    (1'd0          ),
+    .awReady    (              ),
+       
+    .wData      (32'd0         ),
+    .wValid     (1'd0          ),
+    .wReady     (              ),
+       
+    .bResp      (              ),
+    .bValid     (              ),
+    .bReady     (1'd0          )
+);
 /* simple-bus
 Master:
 output        ifu_reqValid,
@@ -95,7 +143,7 @@ output [ 3:0] ifu_wmask,
 input         ifu_respValid,
 output        ifu_respReady,
 input  [31:0] ifu_rdata,
-*/
+
 ysyx_26020055_pROM u_ysyx_26020055_pROM (
     .clk            (clk          ) ,
     .rst            (rst          ) ,
@@ -109,8 +157,10 @@ ysyx_26020055_pROM u_ysyx_26020055_pROM (
     .respReady      (ifu_respReady) ,
     .rdata          (ifu_data     )       
 );
+*/
 
-//更新pc
+
+// ======================== PC 寄存器更新 ============================//
 //如果跳转的话，更新pc为branch_target
     wire [31:0] next_pc ;
     assign next_pc = branch_flag ? branch_target : pc + 4; 
